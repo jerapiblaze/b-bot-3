63e774d5a3999ea1346c16174793b4a775ce1295
jerapiblaze <73020288+jerapiblaze@users.noreply.github.com>
2021-06-19 09:28:28 +0700

    quote cfs is now available

---
+++ modules/serviceWorkers/services/cfs_msg_quoteCfs.js
@@ -0,0 +1,68 @@
+const metadata = {
+    name: 'cfs_msg_quoteCfs',
+    description: 'reply',
+    version: `1.0.0`,
+    author: `jerapiblaze`
+}
+const config = {
+    eventName: `message`,
+    disabled: false
+}
+
+const { fetchMessage } = globalTools
+
+const isSnowflake = (n) => {
+    if (isNaN(n)) {
+        return false
+    }
+    if (n.toString().length === 18) {
+        return true
+    }
+    return false
+}
+
+const exec = async (message) => {
+    if (!message.content.startsWith('/cfs')) return
+
+    const commandArgs = message.content.split(' ')
+    commandArgs.shift()
+
+    const serverPageSettings = cfsPageSettings[message.guild.id]
+    if (!serverPageSettings) return
+
+    const pageSettings = serverPageSettings[commandArgs[0]]
+    if (!pageSettings) {
+        message.lineReplyNoMention('404: no cfs page found')
+        return
+    }
+
+    if (!isSnowflake(commandArgs[1])) {
+        message.lineReplyNoMention('invalid cfsID')
+        return
+    }
+
+    const hallChannel = message.client.channels.cache.find(c => {
+        if (c.name === `${commandArgs[0]}-hall`) {
+            if (c.guild.id === message.guild.id) {
+                return true
+            }
+        }
+        return false
+    })
+    if (!hallChannel) return
+
+    const cfsMessage = await fetchMessage(message.client, hallChannel.id, commandArgs[1])
+    if ((!cfsMessage) || (cfsMessage.deleted)) {
+        message.lineReplyNoMention('broken cfsID: cfs may not exist or be deleted')
+        return
+    }
+
+    message.lineReplyNoMention(cfsMessage.url, cfsMessage.embeds[0])
+    return
+}
+
+module.exports = {
+    metadata,
+    config,
+    exec
+}
